2a386496be2d339fafecf9319189d74746364372
Steven LeBlanc <skleblan@gmail.com>
2020-11-30 16:52:01 -0600

    Add /v0/passphrase. Not working.
    Created new route called passphrase.  Intended to pass simple
    parameter via POST call.  Cannot currently get it working.
    All calls to that route return 404.  Not sure why.
    
    Added notes on how to test w/ wget.  Seem to work for basic hello
    route, but not for passphrase.

--- miscapi.pm
+++ miscapi.pm
@@ -9,4 +9,18 @@ get "/v0/hello" => sub
   return "hello world from miscapi\n";
 };
 
+post "/v0/passphrase" => sub
+{
+  my $passphrase = 'mustang';
+
+  if(body_parameters->get('phrase') eq $passphrase)
+  {
+    return "miscapi received correct passphrase\n";
+  }
+  else
+  {
+    return "bad passphrase sent to miscapi\n";
+  }
+};
+
 1;
